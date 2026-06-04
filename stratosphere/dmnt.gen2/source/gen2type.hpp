5b741cf2f448992b9639ab0c4a174a634b85183d
tomvita <68505331+tomvita@users.noreply.github.com>
2026-06-04 15:11:31 +0800

    0.15

--- stratosphere/dmnt.gen2/source/gen2type.hpp
+++ stratosphere/dmnt.gen2/source/gen2type.hpp
@@ -1,7 +1,7 @@
 #define max_watch_buffer 0x200
 #define max_call_stack 5
 #define stack_check_size 100
-#define GEN2_VERSION "v0.14"
+#define GEN2_VERSION "v0.15"
         typedef struct {
             u64 address:64;
             u32 count:32;
